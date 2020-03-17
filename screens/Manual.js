2db59ee33d81fef89ffe52194a424f3ce74ac76e
waiyanyenaing <wai.yan.ye.naing@fusionsol.com>
2020-03-17 14:10:53 +0630

    update

--- screens/Manual.js
+++ screens/Manual.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
 import {
   Container,
   Icon,
@@ -11,89 +11,89 @@ import {
   Form,
   Picker,
   Button
-} from "native-base";
+} from 'native-base'
 
 export default class Manual extends Component {
   state = {
     lastOnline24: false,
-    selected_language: "key1",
-    selected_topic: "key1",
-    selected_age_Range: "key1",
-    selected_gender: "key1",
+    selected_language: 'key1',
+    selected_topic: 'key1',
+    selected_age_Range: 'key1',
+    selected_gender: 'key1',
     languages: [
       {
-        value: "key1",
-        label: "English"
+        value: 'key1',
+        label: 'English'
       },
       {
-        value: "key2",
-        label: "Myanmar"
+        value: 'key2',
+        label: 'Myanmar'
       }
     ],
     topics: [
       {
-        value: "key1",
-        label: "Gaming"
+        value: 'key1',
+        label: 'Gaming'
       },
       {
-        value: "key2",
-        label: "Food"
+        value: 'key2',
+        label: 'Food'
       },
       {
-        value: "key3",
-        label: "Art"
+        value: 'key3',
+        label: 'Art'
       },
       {
-        value: "key4",
-        label: "Book"
+        value: 'key4',
+        label: 'Book'
       },
       {
-        value: "key5",
-        label: "Technology"
+        value: 'key5',
+        label: 'Technology'
       },
       {
-        value: "key6",
-        label: "Music"
+        value: 'key6',
+        label: 'Music'
       }
     ],
     age_Range: [
       {
-        value: "key1",
-        label: "10-21"
+        value: 'key1',
+        label: '10-21'
       },
       {
-        value: "key2",
-        label: "21-31"
+        value: 'key2',
+        label: '21-31'
       },
       {
-        value: "key2",
-        label: "31-41"
+        value: 'key2',
+        label: '31-41'
       },
       {
-        value: "key2",
-        label: "41-51"
+        value: 'key2',
+        label: '41-51'
       },
       {
-        value: "key2",
-        label: "51-61"
+        value: 'key2',
+        label: '51-61'
       }
     ],
     gender: [
       {
-        value: "key1",
-        label: "Male"
+        value: 'key1',
+        label: 'Male'
       },
       {
-        value: "key2",
-        label: "Female"
+        value: 'key2',
+        label: 'Female'
       }
     ]
-  };
+  }
   onValueChange = (value, key) => {
     this.setState({
       [value]: key
-    });
-  };
+    })
+  }
   render() {
     return (
       <Container style={styles.container}>
@@ -104,7 +104,7 @@ export default class Manual extends Component {
         <Card>
           <CardItem style={styles.card}>
             <Body style={styles.cardBody}>
-              <View style={{ flexDirection: "row", marginVertical: 12 }}>
+              <View style={{ flexDirection: 'row', marginVertical: 12 }}>
                 <View style={{ paddingRight: 20 }}>
                   <Icon
                     type="Ionicons"
@@ -122,7 +122,7 @@ export default class Manual extends Component {
                   />
                 </Right>
               </View>
-              <View style={{ flexDirection: "row", marginVertical: 12 }}>
+              <View style={{ flexDirection: 'row', marginVertical: 12 }}>
                 <View style={{ paddingRight: 20 }}>
                   <Icon
                     type="FontAwesome"
@@ -139,7 +139,7 @@ export default class Manual extends Component {
                     selectedValue={this.state.selected_language}
                     onValueChange={this.onValueChange.bind(
                       this,
-                      "selected_language"
+                      'selected_language'
                     )}
                   >
                     {this.state.languages.map((value, index) => {
@@ -149,12 +149,12 @@ export default class Manual extends Component {
                           label={value.label}
                           value={value.value}
                         />
-                      );
+                      )
                     })}
                   </Picker>
                 </Right>
               </View>
-              <View style={{ flexDirection: "row", marginVertical: 12 }}>
+              <View style={{ flexDirection: 'row', marginVertical: 12 }}>
                 <View style={{ paddingRight: 20 }}>
                   <Icon
                     type="FontAwesome5"
@@ -171,7 +171,7 @@ export default class Manual extends Component {
                     selectedValue={this.state.selected_topic}
                     onValueChange={this.onValueChange.bind(
                       this,
-                      "selected_topic"
+                      'selected_topic'
                     )}
                   >
                     {this.state.topics.map((value, index) => {
@@ -181,12 +181,12 @@ export default class Manual extends Component {
                           label={value.label}
                           value={value.value}
                         />
-                      );
+                      )
                     })}
                   </Picker>
                 </Right>
               </View>
-              <View style={{ flexDirection: "row", marginVertical: 12 }}>
+              <View style={{ flexDirection: 'row', marginVertical: 12 }}>
                 <View style={{ paddingRight: 20 }}>
                   <Icon
                     type="FontAwesome5"
@@ -203,7 +203,7 @@ export default class Manual extends Component {
                     selectedValue={this.state.selected_age_Range}
                     onValueChange={this.onValueChange.bind(
                       this,
-                      "selected_age_Range"
+                      'selected_age_Range'
                     )}
                   >
                     {this.state.age_Range.map((value, index) => {
@@ -213,12 +213,12 @@ export default class Manual extends Component {
                           label={value.label}
                           value={value.value}
                         />
-                      );
+                      )
                     })}
                   </Picker>
                 </Right>
               </View>
-              <View style={{ flexDirection: "row", marginVertical: 12 }}>
+              <View style={{ flexDirection: 'row', marginVertical: 12 }}>
                 <View style={{ paddingRight: 20 }}>
                   <Icon
                     type="FontAwesome5"
@@ -235,7 +235,7 @@ export default class Manual extends Component {
                     selectedValue={this.state.selected_gender}
                     onValueChange={this.onValueChange.bind(
                       this,
-                      "selected_gender"
+                      'selected_gender'
                     )}
                   >
                     {this.state.gender.map((value, index) => {
@@ -245,7 +245,7 @@ export default class Manual extends Component {
                           label={value.label}
                           value={value.value}
                         />
-                      );
+                      )
                     })}
                   </Picker>
                 </Right>
@@ -257,9 +257,9 @@ export default class Manual extends Component {
           block
           rounded
           style={styles.buttonApply}
-          onPress={() => this.props.navigation.navigate("Write")}
+          onPress={() => this.props.navigation.navigate('Write')}
         >
-          <Text style={{ color: "white" }}>Apply</Text>
+          <Text style={{ color: 'white' }}>Apply</Text>
           <Icon
             type="MaterialCommunityIcons"
             name="page-next"
@@ -267,27 +267,27 @@ export default class Manual extends Component {
           />
         </Button>
       </Container>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#E1E2EF"
+    backgroundColor: '#E1E2EF'
   },
   textManual: {
-    fontFamily: "notoSerif-bold",
+    fontFamily: 'notoSerif-bold',
     fontSize: 23,
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: 50,
     marginBottom: 20
   },
   card: {
-    backgroundColor: "#EAEBF9"
+    backgroundColor: '#EAEBF9'
   },
   buttonApply: {
-    backgroundColor: "#9381FF",
+    backgroundColor: '#9381FF',
     marginVertical: 24,
     marginHorizontal: 10
   }
-});
+})
